6aef5721c86dfd33f128c058f82524ad0edfe350
venkatesh <sirisettyvenkatesh@gmail.com>
2024-12-01 15:54:18 +0530

    chnages in css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import HomePage from './jobRinger/pages/HomePage'
 import './App.css'
+import './index.css'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    <HomePage></HomePage>
-    
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
--- src/jobRinger/components/NavBar.jsx
+++ src/jobRinger/components/NavBar.jsx
@@ -1,33 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <>
       <header className="navbar">
-        <div className="logo">
-          <span>Job</span>Ringer
-        </div>
+        <Link to="/" className='links'>
+          <div className="logo">
+            <span>Job</span>Ringer
+          </div>
+        </Link>
+
         <div className="navtitle">
           BRINGING YOU <span>THE PERFECT JOB</span>
         </div>
         <nav>
           <ul>
             <li>
-              <a href="#home" className="button-primary">
+              <a
+                href="https://www.jobringer.com/jobs"
+                className="button-primary"
+              >
                 Jobs
               </a>
             </li>
             <li>
-              <a href="#jobs">Active Employers</a>
+              <a href="https://www.jobringer.com/employers-list.php">
+                Active Employers
+              </a>
             </li>
             <li>
               <a href="#about">Pricing</a>
             </li>
             <li>
-              <button href="#login" className="button2">
+              <button
+                href="https://www.jobringer.com/jobseeker/login"
+                className="button2"
+              >
                 Jobseeker Login
               </button>
-              <button href="#login" className="button2">
+              <button
+                href="https://www.jobringer.com/employer/login"
+                className="button2"
+              >
                 Employer Login
               </button>
             </li>
